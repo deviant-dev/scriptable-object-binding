ac94093854a5f043cd74bafd4948445d9110c7f3
Deviant Dev <dev@deviant.tech>
2018-09-21 12:25:29 +1200

    Clean up SliderBinding to match ToggleBinding

--- Assets/Example/Scripts/SliderBinding.cs
+++ Assets/Example/Scripts/SliderBinding.cs
@@ -2,27 +2,26 @@
 using Deviant.Utils;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Bindings {
 	public class SliderBinding : MonoBehaviour {
-		[SerializeField, FormerlySerializedAs("m_RangeVariable")] private GameVariableFloatRange m_Variable;
-		[SerializeField, FormerlySerializedAs("m_SliderControl")] private Slider m_Control;
+		[SerializeField] private GameVariableFloatRange m_Variable;
+		[SerializeField] private Slider m_Control;
 		[SerializeField] private TextMeshProUGUI m_Label;
 
-		private float m_LastSliderValue;
+		private float m_LastValue;
 
 		private void Start() {
-			m_Variable.Changed += OnVariableChanged;
 			OnVariableChanged(m_Variable.Progress);
+			m_Variable.Changed += OnVariableChanged;
 			if (m_Label) { m_Label.text = m_Variable.Name; }
 		}
 
-		private void OnVariableChanged(float value) { m_LastSliderValue = m_Control.value = m_Variable.Progress; }
+		private void OnVariableChanged(float value) { m_LastValue = m_Control.value = m_Variable.Progress; }
 
 		private void Update() {
-			if (!m_LastSliderValue.Approximately(m_Control.value)) { m_LastSliderValue = m_Variable.Progress = m_Control.value; }
+			if (!m_LastValue.Approximately(m_Control.value)) { m_LastValue = m_Variable.Progress = m_Control.value; }
 		}
 
 		private void OnValidate() {
